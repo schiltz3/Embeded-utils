698e0ca8a2a8c6ecc3ad835fb4b72972f91bf7ca
John Schiltz <johnschiltz2000@gmail.com>
2021-10-15 01:47:04 -0500

    Created header

---
+++ histogram.h
@@ -0,0 +1,31 @@
+/**
+ * @file histogram.h
+ * @author John Schiltz
+ * @date 14 October 2021
+ * @brief File containing the data structures, and public function prototypes for histogram.h
+ */
+#ifndef HISTOGRAM_H
+#define HISTOGRAM_H
+
+#include <stdint.h>
+
+typedef enum
+{
+    NO_ERROR,
+    MEM_ERROR,
+    ZERO_VALUE
+} histogram_error;
+
+typedef struct
+{
+    uint32_t *pHistogramCount;
+    uint8_t *pHistogramPercentage;
+    uint32_t *pBucketLimits;
+    uint8_t num_of_buckets;
+    histogram_error error;
+} histogram_s;
+
+histogram_s *CreateHistogramp(uint32_t *pBuckets, uint8_t *pHistogramPercentage, uint32_t *pHistogramCount, uint8_t histogramLength);
+histogram_error update_histogram(histogram_s *histogram_struct, uint32_t element);
+
+#endif
